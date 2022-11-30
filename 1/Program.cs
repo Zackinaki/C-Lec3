52693773b691cc7be2054e581f8bdc87e4028f8c
IrinchinovZh <exp.bur@gmail.com>
2022-11-30 15:59:04 +0800

    add

---
+++ 1/Program.cs
@@ -0,0 +1,79 @@
+﻿//1 vid
+void Method1()
+{
+    Console.WriteLine("Avtor Zack");
+}
+
+Method1();
+
+//2vid
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+Method2(msg: "Text");
+
+void Method21(string msg,int count)
+{
+    int i=0;
+    while(i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+    
+}
+
+Method21(msg:"Stroka",count:4);
+Method21(count:4,msg:"New Stroka");
+
+//3vid
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year=Method3();
+Console.WriteLine(year);
+
+//4vid
+
+// string Method4(int count, string text)
+// {
+//     int i=0;
+//     string result =String.Empty;
+
+//     while(i<count)
+//     {
+//         result=result+text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)
+{
+    
+    string result =String.Empty;
+    for(int i =0; i<count;i++)
+    
+    {
+        result=result+text;
+        
+    }
+    return result;
+}
+string res=Method4(10,"abcd");
+Console.WriteLine(res);
+
+for(int i=2;i<=10;i++)
+{
+    for(int j=2;j<=10;j++)
+    {
+        Console.WriteLine($"{i}x{j}={i*j}");
+    }
+    Console.WriteLine();
+}
---
+++ 3/Program.cs
@@ -0,0 +1,58 @@
+﻿int[] arr={1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+    int count= array.Length;
+
+    for(int i=0;i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[]array)
+{
+    for(int i=0; i<array.Length;i=i+1)
+    {
+        int minPosition=i;
+
+        for(int j=i+1;j<array.Length;j++)
+        {
+            if(array[j]<array[minPosition])
+            //if(array[j]>array[minPosition])
+            minPosition=j;
+        }
+        int temporary= array[i];
+        array[i]=array[minPosition];
+        array[minPosition]=temporary;
+    }
+}
+
+void SelectionSort1(int[]array)
+{
+    for(int i=0; i<array.Length;i=i+1)
+    {
+        int minPosition=i;
+
+        for(int j=i+1;j<array.Length;j++)
+        {
+            //if(array[j]<array[minPosition])
+            if(array[j]>array[minPosition])
+            minPosition=j;
+        }
+        int temporary= array[i];
+        array[i]=array[minPosition];
+        array[minPosition]=temporary;
+    }
+}
+
+PrintArray(arr);
+
+Console.WriteLine();
+SelectionSort(arr);
+PrintArray(arr);
+
+Console.WriteLine();
+SelectionSort1(arr);
+PrintArray(arr);
